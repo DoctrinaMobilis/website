7ed099692f60bf18f88f74fd53635b8e0fd39bb7
Lukas <lukassander1991@gmail.com>
2024-07-13 12:32:21 +0200

    build ordner ändern

--- doctrina-mobilis-server/server.js
+++ doctrina-mobilis-server/server.js
@@ -14,11 +14,11 @@ const sslOptions = {
 };
 
 // Middleware to serve static files
-app.use(express.static(path.join(__dirname, '../doctrina-mobilis-client/build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../doctrina-mobilis-client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // Test der Datenbankverbindung und Start des Servers
