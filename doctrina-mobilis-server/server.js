38d508d3325799851906bb2d6ef0bef625bbf2b4
Lukas <lukassander1991@gmail.com>
2024-07-13 11:14:02 +0200

    change port to 8080

--- doctrina-mobilis-server/server.js
+++ doctrina-mobilis-server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const sequelize = require('./config/database');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 // SSL-Zertifikatsdateien laden
 const sslOptions = {
